71c440241e48c6113b6ca07efd86492ae879d1fe
sblrm <sabilillah1324@gmail.com>
2025-10-29 17:27:41 +0700

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { MapProvider } from "@/contexts/MapContext";
 import { DestinationsProvider } from "@/contexts/DestinationsContext";
@@ -37,6 +38,7 @@ const App = () => (
             <TooltipProvider>
               <Toaster />
               <Sonner />
+              <Analytics />
               <Routes>
                 <Route path="/" element={<Layout />}>
                   <Route index element={<HomePage />} />
